09c91c576bbe38004a1d0b271899c8e7c37d261c
SimonXPTO <simaodurao@gmail.com>
2025-04-11 15:32:24 +0100

    Added PlayerStruct

--- ClassVsStruct/Program.cs
+++ ClassVsStruct/Program.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿﻿using System;
 
 namespace ClassVsStruct
 {
     public class Program
     {
-        private static void Main(string[] args)
+        private static void Main()
         {
-            Console.WriteLine("Hello LP1!");
+            PlayerClass pc = new PlayerClass() { Health = 100, Armour = 100};
+            PlayerStruct ps = new PlayerStruct() { Health = 100, Armour = 100 };
+
+            KillPlayerClass(pc);
+            KillPlayerStruct(ps);
+
+            Console.WriteLine($"Class : {pc.Health}");
+            Console.WriteLine($"Struct: {ps.Health}");
+        }
+
+        private static void KillPlayerClass(PlayerClass pc)
+        {
+            pc.Health = 0;
+        }
+
+        private static void KillPlayerStruct(PlayerStruct ps)
+        {
+            ps.Health = 0;
         }
     }
 }
