8178e577a2f493a81dc9d987d8538044cd29dd30
SimonXPTO <simaodurao@gmail.com>
2025-04-11 15:29:03 +0100

    Added ClassVsStruct

---
+++ ClassVsStruct/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassVsStruct
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP1!");
+        }
+    }
+}
